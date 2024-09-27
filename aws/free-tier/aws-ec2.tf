e17394505b751b48df74af105c0e8c135b76b9e1
Queue-ri <qriositylog@gmail.com>
2024-09-27 16:07:30 +0900

    feat: Add EC2 SSH ingress/egress rule (#2)

--- aws/free-tier/aws-ec2.tf
+++ aws/free-tier/aws-ec2.tf
@@ -19,13 +19,32 @@ resource "aws_instance" "integrated-server" {
     ami = "ami-0f0646a5f59758444"
     instance_type = "t2.micro"
     key_name = aws_key_pair.make_keypair.key_name
-    vpc_security_group_ids = [ "sg-0fde538436252f879" ]
     associate_public_ip_address = true
+    security_groups = [ aws_security_group.ec2_sg.name ] # id 아님!
 
     root_block_device {
         volume_type = "gp2"
         volume_size = 30
     }
+}
+
+# EC2 Security Rules
+resource "aws_security_group" "ec2_sg" {
+  name = "allow-ssh"
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
 }
 
 # EIP 설정
